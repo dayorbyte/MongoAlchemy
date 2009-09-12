5e3b579958910854ca8322a1c59b1cc14c72e473
Jeff Jenkins <jeff@Phoenix.local>
2009-09-12 12:35:31 -0400

    Adding a more python-like structure to the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='mongo_mapper',
+    version='0.1',
+    description='Object-Object Mapper for Mongo Databases',
+    author='Jeffrey Jenkins',
+    author_email='jeff@qcircles.net',
+    # url='',
+    packages=['mongo_mapper'])
