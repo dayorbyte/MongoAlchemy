12569569a65c8e77e3dae4ae28536572cec249d9
Jeff Jenkins <jeff@qcircles.net>
2010-09-29 07:48:37 -0400

    Updated license, updating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
+
+VERSION = '0.2'
+DESCRIPTION = 'Document-Object Mapper/Toolkit for Mongo Databases'
 
 setup(
-    name='mongo_mapper',
-    version='0.1',
-    description='Object-Object Mapper for Mongo Databases',
+    name='MongoAlchemy',
+    version=VERSION,
+    description=DESCRIPTION,
     author='Jeffrey Jenkins',
+    license='MIT',
     author_email='jeff@qcircles.net',
-    # url='',
-    packages=['mongo_mapper'])
+    url='http://github.com/jeffjenkins/MongoAlchemy/',
+    packages=['mongoalchemy'],
+    install_requires=['pymongo'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
