2d82ea4176a3788cd9f5468a1dc2762664bdb3d0
Jeff Jenkins <jeff@qcircles.net>
2011-09-16 08:07:19 -0400

    fake encoding for python 3 compat

--- test/test_fields.py
+++ test/test_fields.py
@@ -227,6 +227,6 @@ def binary_wrong_type_test_unwrap():
 
 def binary_value_test():
     s = BinaryField()
-    assert s.wrap(Binary(bytes('foo'))) == Binary(bytes('foo'))
-    assert s.wrap(bytes('foo')) == Binary(bytes('foo'))
-    assert s.unwrap(Binary(bytes('foo'))) == Binary(bytes('foo'))
+    assert s.wrap(Binary(bytes('foo'.encode('ascii')))) == Binary(bytes('foo'.encode('ascii')))
+    assert s.wrap(bytes('foo'.encode('ascii'))) == Binary(bytes('foo'.encode('ascii')))
+    assert s.unwrap(Binary(bytes('foo'.encode('ascii')))) == Binary(bytes('foo'.encode('ascii')))
